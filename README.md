fb28e1fc6d1e8b27fc0270786ff44403d8e190de
Etienne Bondot <etienne.bondot@mwm.io>
2023-08-08 13:19:47 +0200

    feat: general update

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
 # crawl-bot
+
+## Setup
+
+```bash
+$ python3 -m venv venv
+$ . venv/bin/activate
+$ pip install --upgrade pip
+```
+
+## Running Redis locally
+
+In the new terminal tab, run the Redis server on your local machine (assuming Linux or macOS) using the following commands:
+
+```bash
+$ wget http://download.redis.io/redis-stable.tar.gz
+$ tar xvzf redis-stable.tar.gz
+$ rm redis-stable.tar.gz
+$ cd redis-stable
+$ make
+```
+
+The compilation is done now. Some binaries are available in the src directory inside redis-stable/ like redis-server (which is the Redis server that you will need to run) and redis-cli (which is the command-line client that you might need to talk to Redis). To run the server globally (from anywhere on your machine) instead of moving every time to the src directory, you can use the following command:
+
+```bash
+$ sudo make install
+```
+
+The binaries of redis-server are now available in your /usr/local/bin directory. You can run the server using the following command:
+
+```bash
+$ redis-server
+```
+
+The server is now running on port 6379 (the default port). Leave it running on a separate terminal. We would need two other new terminal tabs to be able to run the web server and the Celery worker later. But before running the web server, let's integrate Flask with Celery and make our web server ready to run.
+
+## Running the Celery worker
+
+In the new terminal tab, run the following command:
+
+```bash
+(venv) $ celery -A app.celery worker --loglevel=info
+```
+
+where celery is the version of Celery you're using in this tutorial (4.4.1), with the -A option to specify the celery instance to use (in our case, it's celery in the app.py file, so it's app.celery), and worker is the subcommand to run the worker, and --loglevel=info to set the verbosity log level to INFO.
+
+You should see something like
+
+```bash
+celery@myhost.local v5.3.1 (emerald-rush)
+
+macOS-13.4.1-arm64-arm-64bit 2023-08-08 11:42:27
+
+[config]
+.> app:         app:0x11ce28430
+.> transport:   redis://localhost:6379//
+.> results:     rpc://
+.> concurrency: 10 (prefork)
+.> task events: OFF (enable -E to monitor tasks in this worker)
+
+[queues]
+.> celery           exchange=celery(direct) key=celery
+
+
+[tasks]
+  . app.crawl
+
+[2023-08-08 11:42:28,238: WARNING/MainProcess] /opt/homebrew/lib/python3.10/site-packages/celery/worker/consumer/consumer.py:498: CPendingDeprecationWarning: The broker_connection_retry configuration setting will no longer determine
+whether broker connection retries are made during startup in Celery 6.0 and above.
+If you wish to retain the existing behavior for retrying connections on startup,
+you should set broker_connection_retry_on_startup to True.
+  warnings.warn(
+
+[2023-08-08 11:42:28,244: INFO/MainProcess] Connected to redis://localhost:6379//
+[2023-08-08 11:42:28,244: WARNING/MainProcess] /opt/homebrew/lib/python3.10/site-packages/celery/worker/consumer/consumer.py:498: CPendingDeprecationWarning: The broker_connection_retry configuration setting will no longer determine
+whether broker connection retries are made during startup in Celery 6.0 and above.
+If you wish to retain the existing behavior for retrying connections on startup,
+you should set broker_connection_retry_on_startup to True.
+  warnings.warn(
+
+[2023-08-08 11:42:29,438: INFO/MainProcess] mingle: searching for neighbors
+[2023-08-08 11:42:30,449: INFO/MainProcess] mingle: all alone
+[2023-08-08 11:42:30,464: INFO/MainProcess] celery@myhost.local ready.
+```
+
+## Running the Flask web server
+
+In a new terminal run
+
+```bash
+$ python3.10 app.py --url=<url>
+```
+
+The server should be up and running
+
+```bash
+ ✘ mwm-etiennebondot@myhost  ~/Workspace/crawl-bot   main ±  python3.10 app.py --url=https://app.colorpop.ai
+ * Serving Flask app 'app'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:8000
+Press CTRL+C to quit
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 701-049-463
+127.0.0.1 - - [08/Aug/2023 11:44:52] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [08/Aug/2023 11:44:52] "GET /static/css/styles.css HTTP/1.1" 304 -
+127.0.0.1 - - [08/Aug/2023 11:44:56] "GET /static/css/styles.css HTTP/1.1" 304 -
+127.0.0.1 - - [08/Aug/2023 11:44:59] "POST /start_crawling HTTP/1.1" 200 -
+127.0.0.1 - - [08/Aug/2023 11:44:59] "GET /check_progress/b1280952-c534-4d66-8f61-393b81f8f2f3 HTTP/1.1" 200 -
+127.0.0.1 - - [08/Aug/2023 11:45:00] "GET /check_progress/b1280952-c534-4d66-8f61-393b81f8f2f3 HTTP/1.1" 200 -
+```
---
+++ app.py
@@ -0,0 +1,172 @@
+import argparse
+from urllib.parse import urljoin, urlparse
+import json
+
+import matplotlib
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+from celery import Celery
+from flask import Flask, jsonify, render_template, request
+from tabulate import tabulate
+
+from graph import generateGraph
+from meta import extractMeta
+from pagespeed import calculatePageSpeed
+from tags import extractTags
+
+headers = ["URL", "Loading time (s)", "H1 headings",
+           "H2 headings", "Description", "Keywords"]
+
+# Start the web crawler from a given URL
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', dest='url', type=str, help='A given url to crawl')
+args, unknown = parser.parse_known_args()
+
+
+tabulate.PRESERVE_WHITESPACE = True
+
+matplotlib.use('agg')
+
+# app is the Flask application object that you will use to run the web server.
+# celery is the Celery object that you will use to run the Celery worker.
+# Note that the CELERY_BROKER_URL configuration here is set to the Redis server that you're running locally
+# on your machine. You can change this to any other message broker that you want to use.
+# The celery object takes the application name as an argument and sets the broker argument to the one you specified in the configuration.
+# To add the Flask configuration to the Celery configuration, you update it with the conf.update method.
+app = Flask(__name__, static_folder='static')
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379"
+celery = Celery(
+    app.name,
+    backend='rpc://',
+    broker=app.config['CELERY_BROKER_URL']
+)
+celery.conf.update(app.config)
+
+
+def remove_trail_slash(s):
+    if s.endswith('/'):
+        s = s[:-1]
+    return s
+
+
+def web_crawler(url, depth=10):
+    location = urlparse(url)
+    base_url = location.scheme + "://" + location.netloc
+    visited_urls = set()
+    queue = [(url, 0)]
+    results = []
+
+    while queue:
+        (current_url, current_depth) = queue.pop(0)
+
+        print('analysing [' + str(current_depth) + ']: ' + current_url)
+        print(visited_urls)
+
+        if current_url in visited_urls or current_depth > depth:
+            continue
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            visited_urls.add(current_url)
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Extract main tags h1 / h2
+            tags = extractTags(soup)
+
+            # Extract meta tags
+            metadata = extractMeta(soup)
+
+            # Calculate page speed
+            loading_time = calculatePageSpeed(url)
+
+            results.append([
+                current_url,
+                loading_time,
+                tags['h1_headings'],
+                tags['h2_headings'],
+                metadata
+            ])
+
+            links = soup.find_all('a')
+
+            for link in links:
+                href = link.get('href')
+
+                if href and href.startswith(('/', base_url)):
+                    absolute_url = urljoin(base_url, href)
+                    queue.append((absolute_url, current_depth + 1))
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error crawling {current_url}: {e}")
+
+    return results
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', url=args.url)
+
+
+@celery.task(bind=True)
+def crawl(self, url):
+    crawler_results = web_crawler(url)
+
+    table = []
+    urls = []
+    loading_times = []
+
+    for result in crawler_results:
+        table.append([
+            f"{result[0]}",
+            f"{result[1]:.2f}",
+            "\n".join(result[2]),
+            "\n".join(result[3]),
+            result[4]['description'],
+            "\n".join(result[4]['keywords'])
+        ])
+        urls.append(result[0])
+        loading_times.append(result[1])
+
+    chart_file = generateGraph(urls, loading_times)
+    table = tabulate(table, headers=headers,
+                     tablefmt="html", numalign="right")
+    total = len(crawler_results)
+    average_loading_time = f"{sum(loading_times) / len(loading_times):.2f}"
+
+    response = dict(
+        chart_file=chart_file,
+        table=table,
+        total=total,
+        average_loading_time=average_loading_time
+    )
+
+    self.update_state(state='SUCCESS', meta={'result': response})
+
+    return response
+
+
+@app.route('/start_crawling', methods=['POST'])
+def start_crawling():
+    data = request.get_json()
+    task = crawl.apply_async(args=[data['url']])
+    return jsonify({'task_id': task.id})
+
+
+@app.route('/check_progress/<task_id>', methods=['GET'])
+def check_progress(task_id):
+    task = crawl.AsyncResult(task_id)
+
+    if task.state == 'SUCCESS':
+        result = task.result
+    else:
+        result = None
+
+    return jsonify({"state": task.state, "result": result})
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
